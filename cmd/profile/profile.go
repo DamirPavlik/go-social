f2a2bf7f2d2d82c22d9cc6572c7692ea7e550a5d
DamirPavlik <damir.pavlik@gmail.com>
2025-02-05 20:20:54 +0100

    feat: send friendship status to view

--- cmd/profile/profile.go
+++ cmd/profile/profile.go
@@ -5,35 +5,62 @@ import (
 	"database/sql"
 	"html/template"
 	"log"
-	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
-type User struct {
-	ID       int
-	Username string
+type ProfileData struct {
+	ID         int
+	Username   string
+	Friendship string
 }
 
 func GetProfile(c echo.Context, db *sql.DB, tmpl *template.Template) error {
-	userID, err := strconv.Atoi(c.Param("id"))
+	userID, _ := GetCurrentUser(c, db)
+	profileId, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.String(http.StatusBadRequest, "invalid param id")
+		return render.RenderTemplate(c, tmpl, "error", "invalid profile id")
 	}
 
-	var user User
-	err = db.QueryRow("SELECT id, username FROM users WHERE id = $1", userID).Scan(&user.ID, &user.Username)
+	var profile ProfileData
+	err = db.QueryRow("SELECT id, username FROM users WHERE id = $1", profileId).Scan(&profile.ID, &profile.Username)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			log.Println("jeb mi mater", err)
-			return render.RenderTemplate(c, tmpl, "error", "user not found")
+		return render.RenderTemplate(c, tmpl, "error", "user not found")
+	}
+
+	var status sql.NullString
+	err = db.QueryRow("SELECT status FROM friend_request WHERE (sender_id = $1 AND reciever_id = $2) OR (sender_id = $2 AND reciever_id = $1)", userID, profileId).Scan(&status)
+	if err == sql.ErrNoRows {
+		profile.Friendship = "none"
+	} else if err == nil {
+		if status.String == "accepted" {
+			profile.Friendship = "friends"
+		} else if status.String == "pending" {
+			profile.Friendship = "pending"
+		} else if status.String == "declined" {
+			profile.Friendship = "declined"
 		}
-		log.Println("koji kurac", err)
+	} else {
 		return render.RenderTemplate(c, tmpl, "error", "db err")
 	}
+	log.Println("status: ", status.String)
 
-	return render.RenderTemplate(c, tmpl, "profile", user)
+	return render.RenderTemplate(c, tmpl, "profile", profile)
+	// userID, err := strconv.Atoi(c.Param("id"))
+
+	// var user User
+	// err = db.QueryRow("SELECT id, username FROM users WHERE id = $1", userID).Scan(&user.ID, &user.Username)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		log.Println("jeb mi mater", err)
+	// 		return render.RenderTemplate(c, tmpl, "error", "user not found")
+	// 	}
+	// 	log.Println("koji kurac", err)
+	// 	return render.RenderTemplate(c, tmpl, "error", "db err")
+	// }
+
+	// return render.RenderTemplate(c, tmpl, "profile", user)
 }
 
 func GetCurrentUser(c echo.Context, db *sql.DB) (userId int, username string) {
