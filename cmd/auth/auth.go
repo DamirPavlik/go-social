96e1a7924c13cf8e73a87bd2be58b1d4186cb930
DamirPavlik <damir.pavlik@gmail.com>
2025-02-07 20:07:13 +0100

    feat: add indentifier and email and profile photo to auth

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -3,33 +3,76 @@ package auth
 import (
 	"chat-go-htmx/cmd/render"
 	"database/sql"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
+// type User struct {
+// 	ID       int    `json:"id"`
+// 	Username string `json:"username"`
+// 	Password string `json:"password"`
+// }
+
+const uploadDir = "../uploads/profile_pictures/"
 
 func RegisterUser(c echo.Context, db *sql.DB, tmpl *template.Template) error {
 	username := c.FormValue("username")
+	email := c.FormValue("email")
 	password := c.FormValue("password")
 
-	if username == "" || password == "" {
-		return render.RenderTemplate(c, tmpl, "error", "Username and password cannot be empty")
+	if username == "" || email == "" || password == "" {
+		return render.RenderTemplate(c, tmpl, "error", "All fields are required")
+	}
+
+	file, err := c.FormFile("profile_picture")
+	profilePicture := "default.jpg"
+
+	if err == nil {
+		src, err := file.Open()
+		if err != nil {
+			log.Println("err src: ", err)
+			return render.RenderTemplate(c, tmpl, "error", "Error opening file")
+		}
+		defer src.Close()
+
+		if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+			os.MkdirAll(uploadDir, 0755)
+		}
+
+		ext := filepath.Ext(file.Filename)
+		filename := fmt.Sprintf("%d%s", time.Now().UnixNano(), ext)
+		filePath := filepath.Join(uploadDir, filename)
+
+		dst, err := os.Create(filePath)
+		if err != nil {
+			return render.RenderTemplate(c, tmpl, "error", "Error saving file")
+		}
+		defer dst.Close()
+
+		if _, err = dst.ReadFrom(src); err != nil {
+			return render.RenderTemplate(c, tmpl, "error", "Error writing file")
+		}
+
+		profilePicture = filename
 	}
 
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
-	_, err := db.Exec("INSERT INTO users(username, password_hash, created_at) VALUES ($1, $2, $3)", username, hashedPassword, time.Now())
+
+	_, err = db.Exec(
+		"INSERT INTO users(username, email, password_hash, profile_picture, created_at) VALUES ($1, $2, $3, $4, $5)",
+		username, email, hashedPassword, profilePicture, time.Now(),
+	)
 
 	if err != nil {
-		return render.RenderTemplate(c, tmpl, "error", "Username already taken")
+		return render.RenderTemplate(c, tmpl, "error", "Username or email already taken")
 	}
 
 	c.SetCookie(&http.Cookie{
@@ -42,18 +85,20 @@ func RegisterUser(c echo.Context, db *sql.DB, tmpl *template.Template) error {
 }
 
 func LoginUser(c echo.Context, db *sql.DB, tmpl *template.Template) error {
-	username := c.FormValue("username")
+	identifier := c.FormValue("identifier")
 	password := c.FormValue("password")
 
-	if username == "" || password == "" {
-		return render.RenderTemplate(c, tmpl, "error", "Username and password cannot be empty")
+	if identifier == "" || password == "" {
+		return render.RenderTemplate(c, tmpl, "error", "Username/email and password cannot be empty")
 	}
 
 	var storedHash string
-	err := db.QueryRow("SELECT password_hash FROM users WHERE username = $1", username).Scan(&storedHash)
+	var username string
+	err := db.QueryRow("SELECT password_hash, username FROM users WHERE username = $1 OR email = $1", identifier).Scan(&storedHash, &username)
 
 	if err != nil {
-		return render.RenderTemplate(c, tmpl, "error", "Invalid username or password")
+		log.Println("err logging in: ", err)
+		return render.RenderTemplate(c, tmpl, "error", "invalid username/email or password")
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(storedHash), []byte(password)); err != nil {
