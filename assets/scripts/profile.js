edcdf814647cb4c646e900d288c4de618c844e47
DamirPavlik <damir.pavlik@gmail.com>
2025-02-20 19:14:45 +0100

    feat: add custom file upload script

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -1,1 +1,5 @@
 setupPopup(".btn.btn-edit", ".edit-overlay");
+document.getElementById("profile_picture").addEventListener("change", function() {
+    let label = document.querySelector(".custom-file-upload");
+    label.textContent = this.files[0] ? this.files[0].name : "Choose a file";
+});
