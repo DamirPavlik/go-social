32597cb721ffa9a422374652dd4bfe8b0ca1778d
DamirPavlik <damir.pavlik@gmail.com>
2025-03-10 19:57:10 +0100

    feat: add comment squil

---
+++ sql/schema/009_comments.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+-- +goose Down
+DROP TABLE comments;
